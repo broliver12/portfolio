5afeabecb0669a8fb94fe0734d01c4f8bc096f06
Oliver Straszynski <oliver.strasz@gmail.com>
2023-02-19 12:36:44 -0500

    Update theme, content, images

--- src/components/homepage/about/AboutSection.js
+++ src/components/homepage/about/AboutSection.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './AboutSection.css'
 import '../../Button.css'
+import './Cube.css'
 import ct from '../../../content/AboutContent.js'
 
 /**
@@ -13,7 +14,59 @@ import ct from '../../../content/AboutContent.js'
 function AboutSection(props) {
   const content = ct()
 
+  const [buttonsEnabled, setButtonsEnabled] = useState(true)
   const [sel, setSelected] = useState(0)
+  const [currentClass, setCurrentClass] = useState('')
+
+  const faceOrder = [
+    'front',
+    'back',
+    'top',
+    'right',
+    'bottom',
+    'left',
+    'front',
+    'back',
+    'top',
+    'right'
+  ]
+  /**
+   * ImageCube Init
+   *
+   * @param {boolean} notInitialCall true if not the initial call
+   */
+  async function changeSide(notInitialCall) {
+    if (buttonsEnabled === false) {
+      return
+    }
+
+    const cube = document.querySelector('.cube')
+    let showClass = ''
+
+    setButtonsEnabled(false)
+    setTimeout( () => {
+      setButtonsEnabled(true)
+    }, 1000)
+
+    if (notInitialCall === true) {
+      showClass = 'show-' + faceOrder[sel + 1]
+      setSelected(sel + 1)
+    } else if (notInitialCall === false) {
+      showClass = 'show-' + faceOrder[sel - 1]
+      setSelected(sel - 1)
+    } else {
+      showClass = 'show-' + faceOrder[sel]
+    }
+    if (currentClass) {
+      cube.classList.remove(currentClass)
+    }
+    cube.classList.add(showClass)
+    setCurrentClass(showClass)
+  }
+
+  useEffect(async () => {
+    await changeSide()
+  }, [])
 
   const images = [
     '/img/oliver_headshot.jpg',
@@ -22,11 +75,15 @@ function AboutSection(props) {
     '/img/indi_toronto.png',
     '/img/oliver_italy.png',
     '/img/oliver_st_emilion.png',
+  ]
+
+  const image2 = [
     '/img/oliver_indi_couch.png',
     '/img/oliver_iroquois_falls.png',
     '/img/oliver_kaslo.png',
-    '/img/oliver_indi_farm.png'
+    '/img/oliver_indi_farm.png',
   ]
+
   return (
     <div id="about">
       <div id="about22" className="aboutSectionTitleBar">
@@ -45,27 +102,53 @@ function AboutSection(props) {
             {content.paragraph_three}
           </div>
         </div>
-        <div className='imageContainer'>=
-
-          <div className="profilePicture" style={{
-            backgroundImage: 'url(' + images[sel] + ')'
-          }}>
-            <span className='arrowIcon fas fa-arrow-left'
-              style={{opacity: (sel > 0) ? 1 : 0}}
-              onClick={() => {
-                if (sel > 0) {
-                  setSelected(sel - 1)
-                }
+        <div className='imageContainer'>
+          <span className='arrowIcon fas fa-arrow-left'
+            style={{opacity: (sel > 0) ? 1 : 0}}
+            onClick={async () => {
+              if (sel > 0) {
+                await changeSide(false)
+              }
+            }} />
+          <div className="scene">
+            <div className="cube">
+              <div className="cube__face cube__face--front" style={{
+                backgroundImage:
+                  'url(' + `${(sel <= 2) ? images[0] : image2[0]}` + ')',
+                backgroundSize: 'contain'
               }} />
-            <span className='arrowIcon fas fa-arrow-right'
-              style={{opacity: (sel < images.length - 1) ? 1 : 0}}
-              onClick={() => {
-                if (sel < images.length - 1) {
-                  setSelected(sel + 1)
-                }
+              <div className="cube__face cube__face--back" style={{
+                backgroundImage:
+                  'url(' + `${(sel <= 5) ? images[1] : image2[1]}` + ')',
+                backgroundSize: 'contain'
               }} />
+              <div className="cube__face cube__face--right" style={{
+                backgroundImage:
+                  'url(' + `${(sel <= 5) ? images[2] : image2[2]}` + ')',
+                backgroundSize: 'contain'
+              }} />
+              <div className="cube__face cube__face--left" style={{
+                backgroundImage: 'url(' + images[3] + ')',
+                backgroundSize: 'contain'
+              }} />
+              <div className="cube__face cube__face--top" style={{
+                backgroundImage:
+                  'url(' + `${(sel <= 5) ? images[4] : image2[3]}` + ')',
+                backgroundSize: 'contain'
+              }} />
+              <div className="cube__face cube__face--bottom" style={{
+                backgroundImage: 'url(' + images[5] + ')',
+                backgroundSize: 'contain'
+              }} />
+            </div>
           </div>
-
+          <span className='arrowIcon fas fa-arrow-right'
+            style={{opacity: (sel < images.length + image2.length - 1) ? 1 : 0}}
+            onClick={async () => {
+              if (sel < images.length + image2.length - 1) {
+                await changeSide(true)
+              }
+            }} />
         </div>
       </div>
     </div>
