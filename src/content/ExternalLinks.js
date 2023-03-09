c3e3c6fcff7ae56a6b3732501859309a0777aa56
Oliver Straszynski <oliver.strasz@gmail.com>
2023-03-09 06:15:51 -0500

    Updates

--- src/content/ExternalLinks.js
+++
@@ -1,8 +0,0 @@
-const links = () => {
-  return {
-    'linkedin_url': 'https://www.linkedin.com/in/ostraszynski/',
-    'github_url': 'https://www.github.com/broliver12/',
-    'resume_url': 'ostraszynski_resume.pdf',
-  }
-}
-export default links
