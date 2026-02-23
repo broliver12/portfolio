a0b04e3f335f8c17611bb979f6cdedcf8ece78a5
Oliver Straszynski <oliver.strasz@gmail.com>
2026-02-23 12:26:54 -0500

    [porfolio] remove mixed content. improve theming and padding, and text wrap in specific situations. File cleanup.

--- src/icon/IconExternal.js
+++ src/icon/IconExternal.js
@@ -9,7 +9,7 @@ import React from 'react'
  */
 const IconExternal = () => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
+    xmlns="https://www.w3.org/2000/svg"
     role="img"
     viewBox="0 0 24 24"
     fill="none"
--- src/page_sections/contact/ContactSection.js
+++ src/page_sections/contact/ContactSection.js
@@ -13,7 +13,7 @@ import CardSurface from '../../components/card_surface/CardSurface'
 
 const DownloadIcon = () => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
+    xmlns="https://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
