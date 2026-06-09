b9825f89591f2c654d3cbec51b3557f5b31779fe
Oliver Straszynski <oliver.strasz@gmail.com>
2026-06-08 20:19:25 -0400

    Ran linter

--- src/page_sections/work/WorkSection.js
+++ src/page_sections/work/WorkSection.js
@@ -35,7 +35,7 @@ function WorkSection() {
             >
               <div className="jobHeader">
                 <div className="jobCompanyBlock">
-                  <div className={index == 0 ? "leagueFrame" : "logoFrame"}>
+                  <div className={index == 0 ? 'leagueFrame' : 'logoFrame'}>
                     {job.logo ?
                       <img
                         className="companyLogo"
